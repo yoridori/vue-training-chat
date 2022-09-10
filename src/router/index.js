162bd62cccc6a1dd983eda0fbb456c969f302661
yoridori <midori.engineer@gmail.com>
2022-09-11 02:23:54 +0900

    github pagesを使いたいためのコミット

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
-// import UserList from "@/views/UserList";
+import UserList from "@/views/UserList";
 import AboutView from "@/views/AboutView";
-// import ChatBoard from "@/views/ChatBoard";
-// import LoginView from "@/views/LoginView";
-// import SignUp from "@/views/SignUp";
+import ChatBoard from "@/views/ChatBoard";
+import LoginView from "@/views/LoginView";
+import SignUp from "@/views/SignUp";
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'UserList',
-  //   component: UserList
-  // },
+  {
+    path: '/',
+    name: 'UserList',
+    component: UserList
+  },
   {
     path: '/',
     name: 'About',
@@ -22,21 +22,21 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: AboutView
   },
-  // {
-  //   path: '/chat',
-  //   name: 'ChatBoard',
-  //   component: ChatBoard
-  // },
-  // {
-  //   path: '/login',
-  //   name: 'LoginView',
-  //   component: LoginView
-  // },
-  // {
-  //   path: '/signUp',
-  //   name: 'SignUp',
-  //   component: SignUp
-  // },
+  {
+    path: '/chat',
+    name: 'ChatBoard',
+    component: ChatBoard
+  },
+  {
+    path: '/login',
+    name: 'LoginView',
+    component: LoginView
+  },
+  {
+    path: '/signUp',
+    name: 'SignUp',
+    component: SignUp
+  },
 ]
 
 const router = new VueRouter({
