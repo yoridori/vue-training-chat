07aa8dbe0b24dd0b79fea361e2e57b7314286574
yoridori <midori.engineer@gmail.com>
2022-09-11 02:25:41 +0900

    github pagesを使いたいためのコミット

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
     component: UserList
   },
   {
-    path: '/',
+    path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
