d8a338f7265506f5006ca58f5df45e7de5d9d416
yoridori <midori.engineer@gmail.com>
2022-09-11 01:47:12 +0900

    github pagesを使いたいためのコミット

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
-import UserList from "@/views/UserList";
-// import AboutView from "@/views/AboutView";
+// import UserList from "@/views/UserList";
+import AboutView from "@/views/AboutView";
 // import ChatBoard from "@/views/ChatBoard";
 // import LoginView from "@/views/LoginView";
 // import SignUp from "@/views/SignUp";
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'UserList',
+  //   component: UserList
+  // },
   {
     path: '/',
-    name: 'UserList',
-    component: UserList
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: AboutView
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: AboutView
-  // },
   // {
   //   path: '/chat',
   //   name: 'ChatBoard',
