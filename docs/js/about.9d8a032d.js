7078da1e2eb3acaf942302ca930fbcd1aa7fb81f
yoridori <midori.engineer@gmail.com>
2022-09-10 16:11:41 +0900

    readme削除

--- docs/js/about.9d8a032d.js
+++ docs/js/about.9d8a032d.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkvue_training_chat"]=self["webpackChunkvue_training_chat"]||[]).push([[443],{1272:function(n,t,u){u.r(t),u.d(t,{default:function(){return c}});var a=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],i=u(1001),s={},r=(0,i.Z)(s,a,e,!1,null,null,null),c=r.exports}}]);
-//# sourceMappingURL=about.fea90572.js.map
+//# sourceMappingURL=about.9d8a032d.js.map
