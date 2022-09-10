a98811f8fc36044d4c70aa9cf7163923e237ac39
yoridori <midori.engineer@gmail.com>
2022-09-11 01:44:21 +0900

    github pagesを使いたいためのコミット

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 import UserList from "@/views/UserList";
-import AboutView from "@/views/AboutView";
-import ChatBoard from "@/views/ChatBoard";
-import LoginView from "@/views/LoginView";
-import SignUp from "@/views/SignUp";
+// import AboutView from "@/views/AboutView";
+// import ChatBoard from "@/views/ChatBoard";
+// import LoginView from "@/views/LoginView";
+// import SignUp from "@/views/SignUp";
 Vue.use(VueRouter)
 
 const routes = [
@@ -14,29 +14,29 @@ const routes = [
     name: 'UserList',
     component: UserList
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: AboutView
-  },
-  {
-    path: '/chat',
-    name: 'ChatBoard',
-    component: ChatBoard
-  },
-  {
-    path: '/login',
-    name: 'LoginView',
-    component: LoginView
-  },
-  {
-    path: '/signUp',
-    name: 'SignUp',
-    component: SignUp
-  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: AboutView
+  // },
+  // {
+  //   path: '/chat',
+  //   name: 'ChatBoard',
+  //   component: ChatBoard
+  // },
+  // {
+  //   path: '/login',
+  //   name: 'LoginView',
+  //   component: LoginView
+  // },
+  // {
+  //   path: '/signUp',
+  //   name: 'SignUp',
+  //   component: SignUp
+  // },
 ]
 
 const router = new VueRouter({
