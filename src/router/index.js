01b991575d9836dc11e5e8281cdb706883aefdc4
yoridori <midori.engineer@gmail.com>
2022-09-11 01:10:33 +0900

    test

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 import UserList from "@/views/UserList";
+import AboutView from "@/views/AboutView";
 import ChatBoard from "@/views/ChatBoard";
 import LoginView from "@/views/LoginView";
 import SignUp from "@/views/SignUp";
@@ -19,7 +20,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
   },
   {
     path: '/chat',
