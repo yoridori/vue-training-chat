f4f7c29bb9e50b9ba2e78ab6c35b9b9e737cdb7c
yoridori <midori.engineer@gmail.com>
2022-09-10 16:17:04 +0900

    vue.config.js修正

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
-})
-
-module.exports = {
+  ],
   outputDir: 'docs',
   assetsDir: './',
   publicPath: './',
-}
+})
